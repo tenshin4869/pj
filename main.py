8738c8a6d8735c5bb5ad5ffc4ca28fc59607e5c6
tenshin <ten20031017-sty@ezweb.ne.jp>
2024-11-19 14:25:55 +0900

    add: 差分画像の表示

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ if not cap.isOpened():
     print("カメラを開けませんでした。デバイスを確認してください。")
     exit()
 
-
+# 平均フレーム（基準フレーム）を初期化
+avg = None
 
 while True:
     # フレームを取得
@@ -19,8 +20,15 @@ while True:
     # グレースケールに変換
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # グレースケール画像を表示
-    cv2.imshow("Gray Frame", gray)
+    if avg is None:
+        avg = gray.copy().astype("float")
+        continue
+
+    #現在のフレームとの差分を計算
+    cv2.accumulateWeighted(gray, avg, 0.6)  # 移動平均
+    frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))  # 差分
+
+    cv2.imshow("Frame Delta", frameDelta)
 
     # ESCキーで終了
     if cv2.waitKey(1) & 0xFF == 27:
